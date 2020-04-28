75e718841f4041550f230b6f95bdf8f06b4d48f8
Willslimaa <williansantos38@gmail.com>
2020-04-27 22:20:23 -0300

    Novos exercicios

--- 2-8.js
+++ 2-8.js
@@ -1,13 +1,22 @@
 // Faça um algoritmo que receba um texto de entrada e diga se a primeira letra é
 // maiúscula ou não
 
-var user = require('readline-sync');
-var palavra = user.question("Insira a palavra!");
-var letra = palavra.charCodeAt(0);
+// var user = require('readline-sync');
+// var palavra = user.question("Insira a palavra!");
+// var letra = palavra.charCodeAt(0);
 
 
-if(65>=letra<=90){
-    console.log("Sua letra e minuscula!");
-}else if(91>=letra<=122){
-    console.log("Sua letra e maiuscula!");
-}
+// if(65>=letra<=90){
+//     console.log("Sua letra e minuscula!");
+// }else if(91>=letra<=122){
+//     console.log("Sua letra e maiuscula!");
+// }
+
+
+var rs = require('readline-sync')
+var texto = rs.question('Digite uma palavra ')
+var letra = texto.charAt(0)
+
+if (letra == letra.toUpperCase()){
+    console.log('A primeira letra é maiuscula ')
+} else (console.log('A primeira letra é minuscula '))

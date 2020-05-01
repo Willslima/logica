722be4cf8b3d0fda7ec8e7465827d7646b5ba609
Willslimaa <williansantos38@gmail.com>
2020-04-30 22:04:31 -0300

    30-4 exercicio de array

---
+++ teste.js
@@ -0,0 +1,27 @@
+
+// function fazerCafe(){
+//     console.log('Ferver a água');
+//     console.log('Moer café');
+//     console.log('Por pó no filtro');
+//     console.log('Colocar o coador no bule');
+//     console.log('Por a água no filtro');
+//     console.log('Esperar filtrar');
+//     console.log('Servir o café');
+// }
+
+// fazerCafe();
+
+// function funcaoEstranha(x,y){
+//     return x
+// }
+// console.log(funcaoEstranha(5,10))
+
+function ehPar(x){
+    return x % 2 !== 0
+}
+var numero = ehPar(6)
+if(numero){
+    console.log('Eh par')
+} else{
+    console.log('Não é par')
+}

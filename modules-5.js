78e8c5fe50618ddf42d035aa7b586da4ae1d8195
Willslimaa <williansantos38@gmail.com>
2020-05-07 22:53:52 -0300

    finalizando exercicios de modules

--- modules-5.js
+++ modules-5.js
@@ -13,3 +13,14 @@
 //   let ageFromString = new AgeFromDateString('1987-01-08').age;
 //   console.log("value from ageFromString", ageFromString);
 //   // output: 30 (at the time of this doc)
+
+var ageCalculator = require('age-calculator');
+  var {AgeFromDateString, AgeFromDate} = require('age-calculator');
+  var rs = require('readline-sync')
+  var ano = rs.questionInt('Insira o seu ano de nascimento: ')
+  var mes = rs.questionInt('Insira o seu mes de nascimento: ')
+  var dia = rs.questionInt('Insira o seu dia de nascimento: ')
+
+ mes = mes - 1 // no caso o modulo, janeiro começa no zero
+  var calc_idade = new AgeFromDate(new Date(ano, mes, dia)).age;
+  console.log(`A sua idade é ${calc_idade}`)
--- modules-6.js
+++ modules-6.js
@@ -1,3 +1,15 @@
 // Escolha um programa que você já fez em atividades passadas e faça o output do
 // terminal ficar colorido com o pacote CHALK: https://www.npmjs.com/package/chalk
 
+var chalk = require('chalk')
+
+var senha = 1234
+
+var usuario = require('readline-sync')
+var senhaDig = usuario.question('Digite a senha ')
+
+if(senhaDig == senha){
+    console.log(chalk.green`ACESSO PERMITIDO `)
+} else {
+    console.log(chalk.red`SENHA INCORRETA !!! `)
+} 
